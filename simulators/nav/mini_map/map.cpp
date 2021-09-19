1a76f5fb6b75ee910bcc47f0b284bddb350c97a4
danieljiang520 <danieldj@umich.edu>
2021-09-19 15:26:44 -0400

    updated map.cpp

--- simulators/nav/mini_map/map.cpp
+++ simulators/nav/mini_map/map.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
+#include <string>
 
 using namespace std;
+
 class Map  {
     public:
         vector<uint8_t> ReadInputImage(){
+            cv::Mat image;
+            image = cv::imread(filename, cv::IMREAD_UNCHANGED);   // Read the file
+            // flatten the mat.
+            uint totalElements = image.total()*image.channels(); // Note: image.total() == rows*cols.
             
         }
 
 
 
     private:
-
+        string filename;
 };
